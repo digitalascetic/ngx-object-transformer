e5a3cb950391704d92091c6c7d0e6990ccfc95ea
Eduard Deza Castellano <ed@ascetic.io>
2018-10-23 17:25:59 +0200

    Fixed export class

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export {PropertyNameMapper} from './property.name.mapper';
 export {PrimaryKeyObject} from './primarykey.object';
 export {SimpleObjectTransformer} from './simple-object.transformer';
 export {ExcludeIfNullTransformer} from './exclude-if-null.transformer';
+export {StringArrayTransformer} from './string-array.transformer';
