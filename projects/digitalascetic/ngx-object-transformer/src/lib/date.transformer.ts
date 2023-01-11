c794affaed8242b7ae0cbf1d363955e50cf1b976
eduarddeza <edezacas@gmail.com>
2023-01-11 14:46:47 +0100

    feat: Be sure obj is Date object

--- projects/digitalascetic/ngx-object-transformer/src/lib/date.transformer.ts
+++ projects/digitalascetic/ngx-object-transformer/src/lib/date.transformer.ts
@@ -1,23 +1,23 @@
-import {ObjectTransformer} from './object.transformer';
-import {format, parse} from 'date-fns';
+import {format, isDate, parse} from 'date-fns';
+import {ObjectTransformer} from '@digitalascetic/ngx-object-transformer';
 
 export class DateTransformer implements ObjectTransformer {
 
   private _format: string;
 
-  constructor(format: string) {
-    this._format = format;
+  constructor(formatDate: string) {
+    this._format = formatDate;
   }
 
-  transformToObject(obj: Date): any {
+  transformToObject(obj: any): any {
     if (!obj) {
       return null;
     }
 
     /* According to https://github.com/date-fns/date-fns/blob/main/docs/unicodeTokens.md#popular-mistakes */
     const formatPattern = this._format.replace(/Y/g, 'y').replace(/D/g, 'd');
-
-    return format(obj, formatPattern);
+    const dateToFormat = !isDate(obj) ? new Date(obj) : obj;
+    return format(dateToFormat, formatPattern);
   }
 
   transformFromObject(objStr: string, type: Function): any {
