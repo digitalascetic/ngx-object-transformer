547c245f33323719b41f0ec7ffe9249ecfe3a68b
eduarddeza <edezacas@gmail.com>
2024-09-13 12:01:01 +0200

    feat: Updated package to angular 18 or higher

--- projects/digitalascetic/ngx-object-transformer/src/public_api.ts
+++ projects/digitalascetic/ngx-object-transformer/src/public_api.ts
@@ -2,11 +2,11 @@
  * Public API Surface of ngx-object-transformer
  */
 
-export {DateTransformer} from './lib/date.transformer';
-export {ObjectTransformer} from './lib/object.transformer';
-export {PropertyAccessorMapper} from './lib/property.accessor.mapper';
-export {PropertyNameMapper} from './lib/property.name.mapper';
-export {PrimaryKeyObject} from './lib/primarykey.object';
-export {SimpleObjectTransformer} from './lib/simple-object.transformer';
-export {ExcludeIfNullTransformer} from './lib/exclude-if-null.transformer';
-export {StringArrayTransformer} from './lib/string-array.transformer';
+export * from './lib/date.transformer';
+export * from './lib/object.transformer';
+export * from './lib/property.accessor.mapper';
+export * from './lib/property.name.mapper';
+export * from './lib/primarykey.object';
+export * from './lib/simple-object.transformer';
+export * from './lib/exclude-if-null.transformer';
+export * from './lib/string-array.transformer';
